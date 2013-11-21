1c1d2e73fc99b58eea7bd125bbee9eacc3be95cf
Yannick Warnier <ywarnier@beeznest.org>
2013-11-21 17:16:22 -0500

    Initial scripts

---
+++ get_live_exam_users_munin_proxy.php
@@ -0,0 +1,62 @@
+#!/usr/bin/php5
+<?php
+$doc = <<<CUT
+=head1 NAME
+
+chamilo_live_exams - Munin plugin to monitor the number of live users taking an exam on Chamilo portals.
+
+=head1 APPLICABLE SYSTEMS
+
+Chamilo 1.* web applications.
+
+=head1 CONFIGURATION
+
+The plugin needs access to the root web directory /var/www (or any other given
+path) in order to scan for Chamilo installations. It also offers a way to 
+define subdirs if your Chamilo portals are generally stored under two folder
+levels inside /var/www/. The plugin also needs the possibility to execute PHP
+on the command line (php-cli package) and to connect to a MySQL database
+(php5-mysql package).
+
+=head1 INTERPRETATION
+
+The plugin shows the number of users giving a live exam at any given time 
+for any Chamilo portal, indicating the URL of the corresponding portal.
+
+=head1 MAGIC MARKERS
+
+  #%# family=auto
+  #%# capabilities=autoconf
+
+=head1 BUGS
+
+None known of
+
+=head1 VERSION
+
+  0.2
+
+=head1 AUTHOR
+
+Yannick Warnier <yannick.warnier@beeznest.com>
+
+=head1 LICENSE
+
+AGPLv3
+
+=cut
+CUT;
+
+if ( !empty($argv[1]) && $argv[1] == 'config') {
+  // Global Munin attr., see http://munin-monitoring.org/wiki/protocol-config
+  if (!is_file('/tmp/get_live_exam_users_config')) {
+    @exec('/root/php/get_live_exam_users.php config');
+  }
+  readfile('/tmp/get_live_exam_users_config');
+  exit;
+}
+if (!is_file('/tmp/get_live_exam_users')) {
+  @exec('/root/php/get_live_exam_users.php');
+}
+readfile('/tmp/get_live_exam_users');
+
